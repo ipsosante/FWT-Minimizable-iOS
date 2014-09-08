450709ff5f3bee6b1423a7310d40b085b64ed3d1
Carlos <carlos@futureworkshops.com>
2014-09-08 12:41:23 +0100

    created classes

---
+++ Example/Pod/Classes/FWTMinimizableBaseViewController.m
@@ -0,0 +1,49 @@
+//
+//  FWTMinimizableBaseViewController.m
+//  FWTMinimizable
+//
+//  Created by Carlos Vidal on 08/09/2014.
+//  Copyright (c) 2014 Carlos. All rights reserved.
+//
+
+#import "FWTMinimizableBaseViewController.h"
+
+@interface FWTMinimizableBaseViewController ()
+
+@end
+
+@implementation FWTMinimizableBaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Example/Pod/Classes/FWTModalInteractiveTransition.m
@@ -0,0 +1,13 @@
+//
+//  FWTModalInteractiveTransition.m
+//  FWTMinimizable
+//
+//  Created by Carlos Vidal on 08/09/2014.
+//  Copyright (c) 2014 Carlos. All rights reserved.
+//
+
+#import "FWTModalInteractiveTransition.h"
+
+@implementation FWTModalInteractiveTransition
+
+@end
