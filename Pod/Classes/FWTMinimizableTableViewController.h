97955a3ce1a60bb9b7327b202994ff64b7157e42
Carlos <carlos@futureworkshops.com>
2014-09-08 17:01:12 +0100

    created minimizable base table view controller class

---
+++ Pod/Classes/FWTMinimizableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWTMinimizableTableViewController.h
+//  FWTMinimizable
+//
+//  Created by Carlos Vidal on 08/09/2014.
+//  Copyright (c) 2014 Carlos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTMinimizableTableViewController : UITableViewController
+
+@end
---
+++ Pod/Classes/FWTMinimizableTableViewController.m
@@ -0,0 +1,33 @@
+//
+//  FWTMinimizableTableViewController.m
+//  FWTMinimizable
+//
+//  Created by Carlos Vidal on 08/09/2014.
+//  Copyright (c) 2014 Carlos. All rights reserved.
+//
+
+#import "FWTMinimizableTableViewController.h"
+
+@interface FWTMinimizableTableViewController ()
+
+@end
+
+@implementation FWTMinimizableTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - UITableViewDataSource methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+@end
