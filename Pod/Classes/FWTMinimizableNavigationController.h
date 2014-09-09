08bf8216a83b4e9cbd6dc51a297e666b8c5fad00
Carlos <carlos@futureworkshops.com>
2014-09-09 21:29:48 +0100

    created base nav controller

---
+++ Pod/Classes/FWTMinimizableNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  FWTMinimizableNavigationController.h
+//  FWTMinimizable
+//
+//  Created by Carlos Vidal on 09/09/2014.
+//  Copyright (c) 2014 Carlos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FWTMinimizableNavigationController : UINavigationController
+
+@end
---
+++ Pod/Classes/FWTMinimizableNavigationController.m
@@ -0,0 +1,49 @@
+//
+//  FWTMinimizableNavigationController.m
+//  FWTMinimizable
+//
+//  Created by Carlos Vidal on 09/09/2014.
+//  Copyright (c) 2014 Carlos. All rights reserved.
+//
+
+#import "FWTMinimizableNavigationController.h"
+
+@interface FWTMinimizableNavigationController ()
+
+@end
+
+@implementation FWTMinimizableNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
